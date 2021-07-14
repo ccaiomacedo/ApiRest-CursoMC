6b7fa89c47cc7fa835f9d5cf3b43139261d263c7
caio macedo <caio.macedo@ucsal.edu.br>
2021-07-14 00:14:47 -0300

    Connecting  database h2

--- src/main/java/com/caiomacedo/cursomc/domain/Category.java
+++ src/main/java/com/caiomacedo/cursomc/domain/Category.java
@@ -1,10 +1,17 @@
 package com.caiomacedo.cursomc.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.io.Serializable;
 import java.util.Objects;
 
+@Entity
 public class Category implements Serializable {//serve para dizer que o objeto pode ser convertido em bytes
     private static final long serialVersionUID=1l;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String nome;
 
